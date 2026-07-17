be4671f1c3599f128306bd08921eba107ac9337f
Rohan Sharma Sitoula <100085662+rohansharmasitoula@users.noreply.github.com>
2026-07-17 12:43:04 +0530

    update

--- src/js/onWindowResize.js
+++
@@ -1,7 +0,0 @@
-import { state } from './state.js';
-
-export function onWindowResize() {
-    state.camera.aspect = window.innerWidth / window.innerHeight;
-    state.camera.updateProjectionMatrix();
-    state.renderer.setSize(window.innerWidth, window.innerHeight);
-}
